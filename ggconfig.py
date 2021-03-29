499aef02dd050ae837c54851506a7c03f6fd99d7
ooz <ooz@users.noreply.github.com>
2021-03-29 22:55:59 +0200

    Add ggconfig

---
+++ ggconfig.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+config = {
+    'site': {
+        'base_url': 'https://oliz.io/lego',
+        'render_root_readme': True,
+        'title': 'Lego Designs',
+        'about_url': 'https://oliz.io/about.html',
+        'csp': '''<meta http-equiv="Content-Security-Policy" content="script-src 'unsafe-inline'">''',
+        'referrer': '''<meta name="referrer" content="no-referrer">'''
+    },
+    'author': {
+        'name': 'oz',
+        'url': 'https://oliz.io'
+    },
+    'social': {
+        'github_url': 'https://github.com/ooz'
+    }
+}
